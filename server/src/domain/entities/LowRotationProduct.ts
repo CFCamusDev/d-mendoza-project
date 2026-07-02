1a748eb6b719e84550265dddbed31ea556e6a954
Cristofer Camus <crisprox900@gmail.com>
2026-07-02 07:55:32 -0500

    feat(domain): add low rotation report entities and dto

---
+++ server/src/domain/entities/LowRotationProduct.ts
@@ -0,0 +1,10 @@
+import { ProductVariant } from "./ProductVariant";
+
+export interface LowRotationProduct {
+  variantId: string;
+  sku: string | null;
+  productName: string;
+  attributes: Record<string, any>;
+  daysWithoutMovement: number;
+  lastMovementDate: Date | null;
+}
